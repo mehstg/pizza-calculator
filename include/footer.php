4d99e762ce812d15ee1ddca2323d092b1db3b0dd
Paul Braham <paul@Office-Mac.chepstow.braham.uk>
2025-01-16 13:07:59 +0000

    fix

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
 
-<nav class="navbar navbar-dark bg-primary">
+<nav class="navbar navbar-dark bg-custom">
     <div class="container-fluid">
         <a class="nav-link link-light" >Version 0.1</a>
     </div>
